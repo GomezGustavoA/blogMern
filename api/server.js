a27bfef62ced63c6cf3ccae5f5c39247c21f825f
GomezGustavoA <gustavo_64_231@hotmail.com>
2024-04-17 11:04:50 -0300

    Conexion con BD

--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const server = express();
+const PORT = process.env.PORT || 5000;
+
+server.use(express.json());
+server.use(cors());
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("connected to mongoDB atlas"))
+  .catch((err) => console.log(err));
+
+server.listen(PORT, () => console.log("server listening on port", PORT));
